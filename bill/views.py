3ee39bb1fdef2ca5febb05cacb9bfbfb84e3e58f
Kalloor, Eric Abraham <eric.kalloor@cerner.com>
2021-01-09 23:15:27 +0530

    Calendar added

--- bill/views.py
+++ bill/views.py
@@ -1,13 +1,14 @@
-from django.http import HttpResponse
+from calendar import monthrange
+
 from django.http import JsonResponse
 from django.shortcuts import render
-
 from pymongo import MongoClient
 
 from milkbasket.secret import MONGO_COLLECTION
 from milkbasket.secret import MONGO_DATABASE
 from milkbasket.secret import MONGO_KEY
 from register.models import Register
+from register.utils import get_register_day_entry
 
 
 def index(request, bill_number=None):
@@ -22,6 +23,22 @@ def index(request, bill_number=None):
             due_transactions = Register.objects.filter(pk__in=bill_metadata['transaction_ids'])
             payment_status = True if due_transactions.filter(paid=1) else False
             context.update({'payment_status': payment_status})
+
+            # Extract months which has due for calendar
+            active_months = due_transactions.dates('log_date', 'month', order='DESC')
+            calendar = [{'month': active_month.strftime('%B'),
+                         'year': active_month.strftime('%Y'),
+                         'week_start_day': [x for x in range(0, active_month.weekday())],
+                         'days_in_month': [{'day': day,
+                                            'data': get_register_day_entry(
+                                                bill_metadata['customer_id'], day=day,
+                                                month=active_month.month,
+                                                year=active_month.year)
+                                            } for day in range(1, (
+                             monthrange(active_month.year, active_month.month)[1]) + 1)]
+                         } for active_month in active_months]
+            context.update({'calendar': calendar, })
+
         return render(request, template, context)
     else:
         template = 'bill/index.html'
