33d39ff58b2013d823e0a441757e0ff39b2e4659
Kalloor, Eric Abraham <eric.kalloor@cerner.com>
2021-01-09 17:54:43 +0530

    bill app view bill and payment status added

--- bill/views.py
+++ bill/views.py
@@ -7,11 +7,22 @@ from pymongo import MongoClient
 from milkbasket.secret import MONGO_COLLECTION
 from milkbasket.secret import MONGO_DATABASE
 from milkbasket.secret import MONGO_KEY
+from register.models import Register
 
 
 def index(request, bill_number=None):
     if bill_number:
-        return HttpResponse("Here's the text of the Web page with Bill Number.")
+        template = 'bill/bill_template.html'
+        context = {
+            'page_title': 'Milk Basket - Bill',
+        }
+        bill_metadata = fetch_bill(bill_number, full_data=True)
+        if bill_metadata:
+            context.update(bill_metadata)
+            due_transactions = Register.objects.filter(pk__in=bill_metadata['transaction_ids'])
+            payment_status = True if due_transactions.filter(paid=1) else False
+            context.update({'payment_status': payment_status})
+        return render(request, template, context)
     else:
         template = 'bill/index.html'
         context = {
@@ -20,19 +31,27 @@ def index(request, bill_number=None):
         return render(request, template, context)
 
 
-def validate_bill(request):
+def validate_bill(request, full_data=False):
     response = {
         'status': 'failed',
     }
     bill_number = request.POST.get("bill-number", None)
     if request.method == "POST" and bill_number:
-        client = MongoClient(
-            f'mongodb+srv://milkbasket:{MONGO_KEY}@cluster0.4wgsn.mongodb.net/{MONGO_DATABASE}?retryWrites=true&w=majority')
-        db = client[MONGO_DATABASE]
-        # Fetch Bill Metadata
-        metadata = db[MONGO_COLLECTION]
-        bill_metadata = metadata.find_one({'bill_number': bill_number}, {'_id': 1})
+        bill_metadata = fetch_bill(bill_number)
         if bill_metadata:
             response.update({'metadata': str(bill_metadata['_id']), 'bill_number': bill_number})
             response['status'] = 'success' if bool(dict(bill_metadata)) else 'failed'
     return JsonResponse(response)
+
+
+def fetch_bill(bill_number, full_data=False):
+    client = MongoClient(
+        f'mongodb+srv://milkbasket:{MONGO_KEY}@cluster0.4wgsn.mongodb.net/{MONGO_DATABASE}?retryWrites=true&w=majority')
+    db = client[MONGO_DATABASE]
+    # Fetch Bill Metadata
+    metadata = db[MONGO_COLLECTION]
+    if full_data:
+        bill_metadata = metadata.find_one({'bill_number': bill_number})
+    else:
+        bill_metadata = metadata.find_one({'bill_number': bill_number}, {'_id': 1})
+    return bill_metadata
