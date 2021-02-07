f58790baf3f097204552046783c9baa7b65871b6
Kalloor, Eric Abraham <eric.kalloor@cerner.com>
2021-02-07 15:19:21 +0530

    Removed migrations

--- register/models.py
+++ register/models.py
@@ -22,7 +22,7 @@ class Tenant(models.Model):
 
 
 class Customer(models.Model):
-    tenant = models.ForeignKey(Tenant, on_delete=models.DO_NOTHING, default=2)
+    tenant = models.ForeignKey(Tenant, on_delete=models.DO_NOTHING, )
     name = models.CharField(max_length=50, unique=True)
     contact = models.CharField(max_length=10, null=True, default=None)
     email = models.CharField(max_length=50, null=True, default=None)
@@ -34,7 +34,7 @@ class Customer(models.Model):
 
 
 class Register(models.Model):
-    tenant = models.ForeignKey(Tenant, on_delete=models.DO_NOTHING, default=2)
+    tenant = models.ForeignKey(Tenant, on_delete=models.DO_NOTHING, )
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
     log_date = models.DateTimeField()
     schedule = models.CharField(max_length=15, null=True, default=None)
@@ -44,21 +44,21 @@ class Register(models.Model):
 
 
 class Expense(models.Model):
-    tenant = models.ForeignKey(Tenant, on_delete=models.DO_NOTHING, default=2)
+    tenant = models.ForeignKey(Tenant, on_delete=models.DO_NOTHING, )
     cost = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.CharField(max_length=1000, null=False, default='Description')
     log_date = models.DateTimeField(null=False, default=datetime.now)
 
 
 class Payment(models.Model):
-    tenant = models.ForeignKey(Tenant, on_delete=models.DO_NOTHING, default=2)
+    tenant = models.ForeignKey(Tenant, on_delete=models.DO_NOTHING, )
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     log_date = models.DateTimeField(auto_now_add=True, null=True)
 
 
 class Balance(models.Model):
-    tenant = models.ForeignKey(Tenant, on_delete=models.DO_NOTHING, default=2)
+    tenant = models.ForeignKey(Tenant, on_delete=models.DO_NOTHING, )
     customer = models.OneToOneField(
         Customer,
         on_delete=models.CASCADE,
@@ -68,7 +68,7 @@ class Balance(models.Model):
 
 
 class Income(models.Model):
-    tenant = models.ForeignKey(Tenant, on_delete=models.DO_NOTHING, default=2)
+    tenant = models.ForeignKey(Tenant, on_delete=models.DO_NOTHING, )
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.CharField(max_length=1000, null=False, default='Description')
     log_date = models.DateTimeField(null=False, default=datetime.now)
