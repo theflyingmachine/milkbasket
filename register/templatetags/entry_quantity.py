bf537a1778bbd95343789c71eb71cda45d10426c
Kalloor, Eric Abraham <eric.kalloor@cerner.com>
2022-07-30 13:17:50 +0530

    Default update for inactive customers past entry

--- register/templatetags/entry_quantity.py
+++ register/templatetags/entry_quantity.py
@@ -4,15 +4,15 @@ register = template.Library()
 
 
 @register.simple_tag()
-def entry_quantity(day, register_entry, ):
-    default_quantity = '___'
+def entry_quantity(day, register_entry, default=None):
+    default_quantity = '___' if not default else default
 
     for entry in register_entry:
         if entry.log_date.day == day:
             if entry.schedule.endswith("yes"):
                 default_quantity = entry.quantity
             else:
-                default_quantity = 'Absent'
+                default_quantity = 'Absent' if not default else default
     try:
         return round(default_quantity)
     except:
