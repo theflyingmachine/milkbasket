97fe38712077bcac5309e60718c6f01f41c27705
Kalloor, Eric Abraham <eric.kalloor@cerner.com>
2023-05-07 10:54:57 +0530

    final commit customer profile

---
+++ register/middleware.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from django.conf import settings
+from django.shortcuts import redirect
+
+
+class SessionExpiryMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.user.is_authenticated:
+            timestamp_format = '%Y-%m-%dT%H:%M:%S.%fZ'
+            last_visit = request.session.get('last_visit', None)
+            now = datetime.now()
+            if last_visit:
+                last_visit = datetime.strptime(last_visit, timestamp_format)
+                days_since_last_visit = (now - last_visit).days
+                if days_since_last_visit > 45:
+                    # If it's been more than 45 days since the last visit, log the user out
+                    request.session.flush()
+                    return redirect(settings.LOGOUT_REDIRECT_URL)
+            else:
+                # Set the last visit time if it's not already set
+                request.session['last_visit'] = now.strftime(timestamp_format)
+
+        response = self.get_response(request)
+        return response
